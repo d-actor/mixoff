1d79c36cb211ccb5fa93a252da60af950743e6a1
Dan Actor <danltactor@gmail.com>
2018-06-12 15:01:49 -0600

    update migration set up user model for follows

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  # acts_as_follower gem, allows for friending/following of useres
+  acts_as_follower
+  acts_as_followable
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
