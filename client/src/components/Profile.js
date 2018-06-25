bbf5057dac99a37bc30804d026e632cd7cf51ab9
Dan Actor <danltactor@gmail.com>
2018-06-25 17:57:07 -0600

    pseudo code / fill out class

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {
+  Segment,
+  Container,
+  Header,
+} from 'semantic-ui-react';
 
 class Profile extends React.Component {
+  state = { user: '' }
 
+  componentDidMount() {
+    // TODO bring in current user and set in state
+  }
+
+  render() {
+    return(
+      <Container>
+        <Header as='h1' textAlign='center'>Profile</Header>
+      </Container>
+    )
+  }
 }
 
 export default Profile;
