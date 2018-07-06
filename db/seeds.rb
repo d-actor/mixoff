db48dffc48e676944aee2a693b88dc091ab1113c
Dan Actor <danltactor@gmail.com>
2018-07-05 21:45:17 -0600

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#seed users
+i = 1
+6.times do
+  User.create(
+    email: "test#{i}@test.com",
+    name: "Test#{i}",
+    spotify_name: "danltactor",
+    password: "password"
+  )
+  i += 1
+end
+
