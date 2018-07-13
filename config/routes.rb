1e7da72c54fbb4927988a6df0a580a8ef0f6fb55
Dan Actor <danltactor@gmail.com>
2018-07-12 18:12:22 -0600

    server side routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,29 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
+
     # spotify routes
     get 'spotify/playlists', to: "spotify#playlists"
     get 'spotify/tracks/:id', to: "spotify#playlist_tracks"
 
     # follow routes
-    # users
+    #   users
     post 'follows/create', to: "follows#create"
     post 'follows/destroy', to: "follows#destroy"
     get '/friends', to: "follows#index_friends"
-    # mixoffs
+    #   mixoffs
     post 'follows/mixoff', to: "follows#follow_mixoff"
     post 'follows/unfollow_mixoff', to: "follows#unfollow_mixoff"
 
     # user routes
     get 'users', to: "users#index"
 
+    # mixoff routes
+    resources :mixoffs
+
   end
 
   #Do not place any routes below this one
   get '*other', to: 'static#index'
 end
+
